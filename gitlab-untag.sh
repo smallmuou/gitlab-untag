8b8d7c2c4dcc15d4c7999d1ef8219b7d65a9dd3a
smallmuou <lvyexuwenfa100@126.com>
2016-12-22 18:37:09 +0800

    延后本地分支删除

--- gitlab-untag.sh
+++ gitlab-untag.sh
@@ -88,9 +88,10 @@ fi
 
 info "Delete tag $tagname ..."
 
-git tag -d $tagname
 git push $repository --delete tag $tagname
 git tag __tmp__
 git push $repository --tags
 git tag -d __tmp__
 git push $repository --delete tag __tmp__
+
+git tag -d $tagname
